670fbdd885026367a5ab330650815800fbdaf118
mohasadaq <85059435+mohasadaq@users.noreply.github.com>
2021-09-25 14:18:48 +0300

    Update ExpenseService.js

--- src/service/ExpenseService.js
+++ src/service/ExpenseService.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const url = "http://localhost:8800/expense";
+const url = "https://petrolmanagement.herokuapp.com/expense";
 
 class ExpenseService {
     getExpenseList(){
