dff5169cf11753b4967370a2d395c1ed9735a3f8
mohasadaq <85059435+mohasadaq@users.noreply.github.com>
2021-09-25 14:27:18 +0300

    Update salesReport.js

--- src/service/salesReport.js
+++ src/service/salesReport.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const url = "http://localhost:8800/transection";
+const url = "https://petrolmanagement.herokuapp.com/transection";
 class SalesReport  {
     getSalesReport(){
         return  axios.get(`${url}/salesReport`);
