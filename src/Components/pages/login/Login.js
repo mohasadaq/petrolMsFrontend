215228f591773acf9cfc5b206b6526fad10a6daa
mohasadaq <85059435+mohasadaq@users.noreply.github.com>
2021-09-25 13:27:39 +0300

    Update Login.js
    login chages

--- src/Components/pages/login/Login.js
+++ src/Components/pages/login/Login.js
@@ -12,7 +12,7 @@ const login_author = ()=>{
   let input =  Appfunction.validate_form_inputs(['empUsername','empPassword'])
   if(input!==null){
       axios
-        .post("http://localhost:8800/Employee/checklogin", {
+        .post("https://petrolmanagement.herokuapp.com/checklogin", {
           empUsername: $("#empUsername").val(),
           empPassword: $("#empPassword").val(),
         })
@@ -57,7 +57,7 @@ const login_author = ()=>{
 
 				<div class="login100-form">
 					<span class="login100-form-title">
-						Member Login
+						 Login
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
@@ -103,94 +103,7 @@ const login_author = ()=>{
                     </ul>
                     </div>
                 
-         {/* <div className="account-pages mt-5 mb-5">
-          <div className="container">
-              <div className="row justify-content-center">
-                  <div className="col-md-8 col-lg-6 col-xl-4">
-                      <div className="card bg-pattern">
-
-                          <div className="card-body p-4">
-                              
-                              <div className="text-center w-75 m-auto">
-                                  <div className="auth-logo">
-                                      <div  className="logo logo-dark text-center">
-                                          <span className="logo-lg">
-                                              <img src="../assets/images/kamil.jpeg" alt="" height="100" width="100"/>
-                                          </span>
-                                      </div>
-                  
-                                      <a href="index.html" className="logo logo-light text-center">
-                                          <span className="logo-lg">
-                                              <img src="../assets/images/logo-light.png" alt="" height="22"/>
-                                          </span>
-                                      </a>
-                                  </div>
-                                  <p className=" mb-4 mt-3"><strong> Petrol Managment System </strong></p>
-                              </div>
-
-
-                                  <div className="mb-3">
-                                      <label  className="form-label">User Name</label>
-                                      <input className="form-control" type="text" id="empUsername" required="" placeholder="Enter your User Name"/>
-                                  </div>
-
-                                  <div className="mb-3">
-                                      <label  className="form-label">Password</label>
-                                      <div className="input-group input-group-merge">
-                                          <input type="password" id="empPassword" className="form-control" placeholder="Enter your password"/>
-                                          <div className="input-group-text" data-password="false">
-                                             <span onClick={() => {
-                                            var type = document.getElementById('empPassword').type;
-                                            if (type == 'password')
-                                                document.getElementById('empPassword').type = 'text'
-                                           else
-                                             document.getElementById('empPassword').type = 'password'    
-                                                
-                                              }} className="password-eye"></span>
-                                          </div>
-                                      </div>
-                                  </div>
-
-                                  
-
-                                  <div className="text-center d-grid">
-                                      <button className="btn btn-primary" onClick={()=>{login_author()}} type="button"> Log In </button>
-                                  </div>
-
-
-                              <div className="text-center">
-                                  <ul className="social-list list-inline mt-3 mb-0">
-                                      <li className="list-inline-item">
-                                          <a href="javascript" className="social-list-item border-primary text-primary"><i className="mdi mdi-facebook"></i></a>
-                                      </li>
-                                      <li className="list-inline-item">
-                                          <a href="javascript" className="social-list-item border-danger text-danger"><i className="mdi mdi-google"></i></a>
-                                      </li>
-                                      <li className="list-inline-item">
-                                          <a href="javascript" className="social-list-item border-info text-info"><i className="mdi mdi-twitter"></i></a>
-                                      </li>
-                                      <li className="list-inline-item">
-                                          <a href="javascript" className="social-list-item border-secondary text-secondary"><i className="mdi mdi-github"></i></a>
-                                      </li>
-                                  </ul>
-                              </div>
-
-                          </div>
-                      </div>
-
-                      <div className="row mt-3">
-                          <div className="col-12 text-center">
-                              <p> <a href="auth-recoverpw.html" className="text-white-50 ms-1">Forgot your password?</a></p>
-                              <p className="text-white-50">Don't have an account? <a href="auth-register.html" className="text-white ms-1"><b>Sign Up</b></a></p>
-                          </div> 
-                      </div>
-                      
-
-                  </div>
-              </div>
-          </div>
-      </div>
-      */}
+         
             </>
         );
 }
